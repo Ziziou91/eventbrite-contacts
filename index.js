fb98964ca0403d391fa642e5ad1e91861a68013f
Ziziou91 <joshgray91@gmail.com>
2019-02-19 10:28:14 +0000

    fix function name in require at index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const {getAllEvents, getAllSignups, getITPIds, filterOnlyAttendees} = require('./lib/eventbriteData.js')
+const {getAllEvents, getAllSignups, getITPAndTasterIds, filterOnlyAttendees} = require('./lib/eventbriteData.js')
 const {contactParser, postContactsToHubspot} = require('./lib/hubspotData')
 
 const sendAttendeesToHubspot = async () => {
     try {
         await getAllEvents()
-                .then( getITPAndTasterIds)
+                .then(getITPAndTasterIds)
                 .then(getAllSignups)
                 .then(filterOnlyAttendees)
                 .then(contactParser)
