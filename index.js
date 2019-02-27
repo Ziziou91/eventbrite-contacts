84050d2cde082279f13d81051f6f3d04c220ea10
Ziziou91 <joshgray91@gmail.com>
2019-02-27 12:58:59 +0000

    refactor contactParser and include default values for eventNames when no argument passed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const {getEventbriteIDs, getAttendees} = require('./lib/eventbriteData.js')
 const {postAttendeesToHubspot} = require('./lib/hubspotData')
 const KEYS = require('./.ignore/keys');
 
-const sendAttendeesToHubspot = async (auth) => {
+const sendAttendeesToHubspot = async (auth, eventNames = ['taster', 'introduction']) => {
     try {
-        const eventIDs = await getEventbriteIDs(auth.eventbrite)
+        const eventIDs = await getEventbriteIDs(auth.eventbrite, eventNames)
         const attendees = await getAttendees(eventIDs, auth.eventbrite)
         postAttendeesToHubspot(attendees, auth.hubspot)
         return attendees
