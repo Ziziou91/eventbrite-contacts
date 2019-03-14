40294338f7e5ae9d1ea7725bb77311b094c414d5
Ziziou91 <joshgray91@gmail.com>
2019-03-14 18:05:43 +0000

    now get ycbm bookings and sends to hubspot

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const {getEventbriteIDs, getAttendees} = require('./lib/eventbriteData.js')
-const {postAttendeesToHubspot} = require('./lib/hubspotData')
+const { getEventbriteIDs, getAttendees } = require('./lib/eventbriteData.js');
+const {
+  postAttendeesToHubspot,
+  eventbriteContactParser,
+  ycbmContactParser
+} = require('./lib/hubspotData');
+const { getYCBMBookings } = require('./lib/youCanBookMe');
 const KEYS = require('./.ignore/keys');
 
-const sendAttendeesToHubspot = async (auth, eventNames = ['taster', 'introduction']) => {
-    try {
-        const eventIDs = await getEventbriteIDs(auth.eventbrite, eventNames)
-        const attendees = await getAttendees(eventIDs, auth.eventbrite)
-        postAttendeesToHubspot(attendees, auth.hubspot)
-        return attendees
-        }
-    catch (error) {
-        return (error);
-    }
-}
-sendAttendeesToHubspot(KEYS)
+const sendAttendeesToHubspot = async (
+  auth,
+  eventNames = ['taster', 'introduction']
+) => {
+  try {
+    const eventIDs = await getEventbriteIDs(auth.eventbrite, eventNames);
+    const attendees = await getAttendees(eventIDs, auth.eventbrite);
+    postAttendeesToHubspot(attendees, eventbriteContactParser, auth.hubspot);
+    return attendees;
+  } catch (error) {
+    return error;
+  }
+};
+
+const sendYCBMBookingsHubspot = async auth => {
+  try {
+    const bookings = await getYCBMBookings(auth.ycbm);
+    postAttendeesToHubspot(bookings, ycbmContactParser, auth.hubspot);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+sendAttendeesToHubspot(KEYS);
+sendYCBMBookingsHubspot(KEYS);
 
 module.exports = {
-    sendAttendeesToHubspot
-}
+  sendAttendeesToHubspot
+};
